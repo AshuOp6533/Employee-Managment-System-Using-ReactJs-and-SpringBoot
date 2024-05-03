10d3b5902f2287376b74fef07753c7dd7198010d
AshuOp6533 <aashusin0015@gmail.com>
2024-05-03 12:35:18 +0530

    connected spring with react

---
+++ employee-management-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+import EmployeeForm from './EmployeeForm'
+
+const App = () => {
+  return (
+    <Fragment>
+        <EmployeeForm/>
+    </Fragment>
+  )
+}
+export default App;
